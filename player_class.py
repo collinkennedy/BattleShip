7de6ca56d638e666553f88febd931b449750a62c
Collin Kennedy <collinjkennedy@gmail.com>
2020-01-29 13:39:15 -0800

    first commit to ShipLocation Class

--- player_class.py
+++ player_class.py
@@ -6,6 +6,8 @@ class Player:
         self.name = self.getNameFromPlayer(otherPlayers)
         self.piece = self.getPieceFromPlayer(otherPlayers, blankCharacter)
 
+
+
     @staticmethod
     def get_name_from_player(otherPlayers: Iterable["Player"]) -> str:
         """get a VALID name form the player.
@@ -18,6 +20,9 @@ class Player:
                 return name
             else:
                 print(f'{name} has already been used. Pick another name.')
+
+    def __str__(self) -> str:
+        return self.name
 
 
 if __name__ == "__main__":

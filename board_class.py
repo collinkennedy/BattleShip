403009cff88c0317de54e0cbd930899cba9e683a
Robert Marchesiello <robbymarchesiello@gmail.com>
2020-01-29 16:16:55 -0800

    created __str__ for board_class

--- board_class.py
+++ board_class.py
@@ -7,5 +7,12 @@ class Board:
                 self.contents.append(blankChar)
         self.blank_char = blankChar
 
+    def __str__(self) -> str:
+        sep = ' ' * max([len(str(self.num_rows)), len(str(self.num_cols))])
+        rep = sep * 2 + sep.join((str(i) for i in range(self.num_cols))) + '\n'
+        for row_index, row in enumerate(self):
+            rep += str(row_index) + sep + sep.join(row) + '\n'
+        return rep
+
 if __name__== "__main__":
     pass

2f4d6bf7b7382e1cce99160af4d421a00a282b95
Collin Kennedy <collinjkennedy@gmail.com>
2020-01-29 16:23:25 -0800

    xd

--- board_class.py
+++ board_class.py
@@ -1,5 +1,5 @@
 class Board:
-    
+
     def __init__(self, numRows: int, numCols: int, blankChar: str) -> None:
         self.contents = []
         for row in range(numRows):
@@ -8,11 +8,12 @@ class Board:
         self.blank_char = blankChar
 
     def __str__(self) -> str:
-        sep = ' ' * max([len(str(self.num_rows)), len(str(self.num_cols))])
-        rep = sep * 2 + sep.join((str(i) for i in range(self.num_cols))) + '\n'
-        for row_index, row in enumerate(self):
-            rep += str(row_index) + sep + sep.join(row) + '\n'
+        sep = ' ' * max([len(str(self.numRows)), len(str(self.numCols))])
+        rep = sep * 2 + sep.join((str(i) for i in range(self.numCols))) + '\n'
+        for rowIndex, row in enumerate(self):
+            rep += str(rowIndex) + sep + sep.join(row) + '\n'
         return rep
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     pass

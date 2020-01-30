f0c583068a5089f6f72e914ff61df6331b948353
Robert Marchesiello <robbymarchesiello@gmail.com>
2020-01-29 16:22:35 -0800

    created iterable and properties for board_class

--- board_class.py
+++ board_class.py
@@ -1,3 +1,6 @@
+from typing import Iterable
+
+
 class Board:
     
     def __init__(self, numRows: int, numCols: int, blankChar: str) -> None:
@@ -8,11 +11,24 @@ class Board:
         self.blank_char = blankChar
 
     def __str__(self) -> str:
-        sep = ' ' * max([len(str(self.num_rows)), len(str(self.num_cols))])
-        rep = sep * 2 + sep.join((str(i) for i in range(self.num_cols))) + '\n'
-        for row_index, row in enumerate(self):
-            rep += str(row_index) + sep + sep.join(row) + '\n'
+        sep = ' ' * max([len(str(self.numRows)), len(str(self.numCols))])
+        rep = sep * 2 + sep.join((str(i) for i in range(self.numCols))) + '\n'
+        for i, row in enumerate(self):
+            rep += str(i) + sep + sep.join(row) + '\n'
         return rep
+
+    def __iter__(str) -> Iterable[List[str]]:
+        return iter(self.contents)
+
+    @property
+    def numCols(self):
+        return len(self.numCols)
+
+    @property
+    def numRows(self):
+        return len(self.numRows)
+
+
 
 if __name__== "__main__":
     pass

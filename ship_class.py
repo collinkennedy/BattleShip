431795fbf2d9983d5ca2795dbda4a5ca135e2a7c
Robert Marchesiello <robbymarchesiello@gmail.com>
2020-01-29 17:28:34 -0800

    created readFileShips to dict

--- ship_class.py
+++ ship_class.py
@@ -2,14 +2,26 @@ from typing import Iterable, Set, Tuple
 import sys
 
 class Ship:
-    def __init__(self):
+    def __init__(self, index :int):
         self.name = self.getShipName(listOfShips)
         self.size = self.getShipSize(listOfShips)
 
     @staticmethod
     def getShipName() -> str:
-        
         pass
+
+    @staticmethod
+    def readFileShips() -> Iterable[Dict]:
+        dictOfShips = {}
+        with open(sys.argv[1]) as file:
+            for i, line in enumerate(file):
+                if i == 0:
+                    continue
+                else:
+                    shipName, shipSize = line.split(" ")
+                    dictOfShips[shipSize] = shipName
+        return dictOfShips
+
 
 
 
